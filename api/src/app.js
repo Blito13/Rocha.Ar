2f11ec47ecb1298bf5cc65e8d6ffcbcb38c54d50
Blito13 <lesta4444@hotmail.com>
2023-01-05 20:21:18 -0300

    firstc

---
+++ api/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+
+const random = require('./routes/')
+const random1 = require('./routes/')
+const random2 = require('./routes/')
+const server = express();
+
+server.name = 'API';
+
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+server.use(random);
+/* server.use(recipe); */
+server.use(random1);
+server.use(random2);
+
+// Error catching endware.
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
